12902541f6ccebaecbeda6f8bec18cdc71c72f1e
AndrewHrabrov <medveddimon623@gmail.com>
2024-10-17 22:54:20 +0300

    task 3

---
+++ lab4.3.py
@@ -0,0 +1,16 @@
+import os
+from inspect import stack
+
+
+def dir_tree(dir_name):
+    stack = [(dir_name, 0)]
+
+    while stack:
+        cur_dir, level = stack.pop()
+        elements = os.listdir(cur_dir)
+
+        for elem in elements:
+            path = os.path.join(cur_dir, elem)
+            if os.path.isdir(path): stack.append((path, level + 1))
+        indent = 4 * level * ' '
+        print(indent.join(map(str, elements)))
