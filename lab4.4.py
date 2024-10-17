99b66b25f1c16ec7656cd94945698f4d554d1e53
AndrewHrabrov <medveddimon623@gmail.com>
2024-10-18 00:26:09 +0300

    task 4

---
+++ lab4.4.py
@@ -0,0 +1,20 @@
+from pathlib import Path, PurePath
+
+
+def find_expansion(dir_name, exp):
+    dir_name = Path(dir_name).resolve()
+    stack = [(dir_name)]
+    files = []
+    while stack:
+        cur_dir = stack.pop()
+        elements = Path(cur_dir).iterdir()
+        for elem in elements:
+            path = PurePath(cur_dir).joinpath(elem)
+            if Path(path).is_file():
+                expansion = PurePath(path).suffix
+                if (expansion == exp):
+                    files.append(PurePath(path).stem)
+            if Path(path).is_dir(): stack.append(path)
+    print(files)
+
+find_expansion('master', '.html')
